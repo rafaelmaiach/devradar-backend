9c7de54ed9ac2d9e07bd69da7693c53a7d3a90ea
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-01-16 01:18:07 +0000

    Adds cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('./config');
 
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./routes');
 const database = require('./database');
@@ -9,6 +10,7 @@ const app = express();
 
 database.connectMongo();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
